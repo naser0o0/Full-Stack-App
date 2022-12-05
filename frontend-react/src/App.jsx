066a1a10591f89c8e6a3ccd07b41ce27a612b465
Marco Kadlubski <marco.kdlbsk@mailbox.org>
2022-12-05 16:49:50 +0100

    Fix Routing Error

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import {RouterProvider} from 'react-router-dom';
+import { RouterProvider } from "react-router-dom";
 import { router } from "./Routing/Router";
 
 function App() {
   return (
     <div>
       <h1>Hallo App js</h1>
-      <RouterProvider route={router} />
+      <RouterProvider router={router} />
     </div>
   );
 }
