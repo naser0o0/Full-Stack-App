5bfff2f9ed6457a04920ee2559c1a77d32fb0b10
Marco Kadlubski <marco.kdlbsk@mailbox.org>
2022-12-07 17:02:00 +0100

    zwischenspeichern

--- frontend-react/src/Components/Forms/RegisterForm.jsx
+++ frontend-react/src/Components/Forms/RegisterForm.jsx
@@ -1,44 +1,61 @@
-import React, { useState } from 'react';
-import Layout from '../../layout/Layout';
+import React, { useState } from "react";
+import Layout from "../../layout/Layout";
 
+export default function RegisterForm() {
+  const date = new Date();
 
-export default function RegisterForm(){
-
-    const date = new Date()
-
-const newUserSchema = {
+  const newUserSchema = {
     firstname: "",
     lastname: "",
     email: "",
     password: "",
-    token: date.getTime()
-}
+    token: date.getTime(),
+  };
 
-const [newUser, setNewUser]=useState(newUserSchema)
+  const [newUser, setNewUser] = useState(newUserSchema);
 
-function handleRegisterForm (e){
+  function handleRegisterForm(e) {}
 
+  function handleRegistration(e) {}
 
-
-}
-
-    function handleRegistration(e){
-
-    }
-    
-
-    return(
-        <Layout>
-            <div>
-            <h1>Registration Form</h1>
-        <form>
-            <input type="text" name="firstname" placeholder="firstname" required onChange={handleRegisterForm}/><br/>
-            <input type="text" name="lastname" placeholder="lastname" required onChange={handleRegisterForm}/><br/>
-            <input type="email" name="email" placeholder="email" required onChange={handleRegisterForm}/><br/>
-            <input type="password" name="password" placeholder="password" required onChange={handleRegisterForm}/><br/>
-            <button onClick={handleRegistration}>Register</button>
-        </form>
-        </div>
-        </Layout>
-    )
+  return (
+    <div>
+      <h1>Registration Form</h1>
+      <form>
+        <input
+          type="text"
+          name="firstname"
+          placeholder="firstname"
+          required
+          onChange={handleRegisterForm}
+        />
+        <br />
+        <input
+          type="text"
+          name="lastname"
+          placeholder="lastname"
+          required
+          onChange={handleRegisterForm}
+        />
+        <br />
+        <input
+          type="email"
+          name="email"
+          placeholder="email"
+          required
+          onChange={handleRegisterForm}
+        />
+        <br />
+        <input
+          type="password"
+          name="password"
+          placeholder="password"
+          required
+          onChange={handleRegisterForm}
+        />
+        <br />
+        <button onClick={handleRegistration}>Register</button>
+      </form>
+    </div>
+  );
 }
